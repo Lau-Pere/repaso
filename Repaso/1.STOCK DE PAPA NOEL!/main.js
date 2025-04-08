7b3292d0784152fe85acd63bce3b33616ee5cced
Laura <pereyra.laura.a@gmail.com>
2025-04-08 20:37:51 +0200

    main.js

--- Repaso/1.STOCK DE PAPA NOEL!/main.js
+++ Repaso/1.STOCK DE PAPA NOEL!/main.js
@@ -1,18 +1,15 @@
 let stock = 150;
 
 while (stock > 0) {
-    let regalosRepartidos = parseInt(prompt("¿Cuántos regalos has repartido hoy?")); // Pregunta a Papá Noel
+    let regalosRepartidos = parseInt(prompt("¿Cuántos regalos has repartido hoy?")); 
     
     if (regalosRepartidos > stock) {
-        alert("No puedes repartir más de lo que queda: " + stock);
-    } else {
+        alert("No puedes repartir más de lo que queda: " + stock);} 
+    else {
         stock = stock - regalosRepartidos;
         alert("Quedan " + stock + " regalos");
         
         if (stock < 100 && stock > 0) {
-            alert("Ojo, quedan menos de 100 regalos");
-        }
-    }
-}
+            alert("Ojo, quedan menos de 100 regalos");}}}
 
 alert("Misión cumplida. ¡Hasta el año que viene!");
