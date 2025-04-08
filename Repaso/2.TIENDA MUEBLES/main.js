8557196f952ada4e93b59904952d66fb12f233b8
Laura <pereyra.laura.a@gmail.com>
2025-04-08 20:39:23 +0200

    main.js

--- Repaso/2.TIENDA MUEBLES/main.js
+++ Repaso/2.TIENDA MUEBLES/main.js
@@ -2,15 +2,8 @@ let esSilla = prompt("¿Es una silla? (s/n)").toLowerCase();
 let esIKEA = prompt("¿Es de IKEA? (s/n)").toLowerCase();
 
 if (esSilla === "s") {
-    if (esIKEA === "s") {
-        alert("El mueble es: Silla IKEA");
-    } else {
-        alert("El mueble es: Silla Mackintosh");
-    }
-} else {
-    if (esIKEA === "s") {
-        alert("El mueble es: Mesa IKEA");
-    } else {
-        alert("El mueble es: Mesa Roble");
-    }
-}
+    if (esIKEA === "s") { alert("El mueble es: Silla IKEA");} 
+    else { alert("El mueble es: Silla Mackintosh");}} 
+else {
+    if (esIKEA === "s") { alert("El mueble es: Mesa IKEA");} 
+    else { alert("El mueble es: Mesa Roble");}}
